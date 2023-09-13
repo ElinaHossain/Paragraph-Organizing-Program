3635680efd7469dd92723272c913fae0d2a63474
Elina <81986341+ElinaHossain@users.noreply.github.com>
2023-09-12 21:47:16 -0700

    Add files via upload

---
+++ POP.java
@@ -0,0 +1,95 @@
+//Paragraph Organizing Program
+
+import java.util.Scanner;
+
+public class POP {
+     public static void main(String[] args) {
+          //Storage
+          Scanner input = new Scanner(System.in);
+          //Input part 1
+          String linesText;
+          if (args.length == 0) {
+               System.out.print("Enter the number of lines to type : ");
+               linesText = input.nextLine();
+          } 
+          else {
+               linesText = args[0];
+          }
+          //Input part 2
+          Scanner textScanner = new Scanner(linesText);
+          if (!textScanner.hasNextInt()) {
+               System.out.print("Error: non-integer input");
+               System.exit(1);
+          }
+          int lines = textScanner.nextInt();
+          if (lines < 1 || lines > 5) {
+               System.out.print("Error: number of lines must be between 1 and 5");
+               System.exit(1);
+          }
+          //Input part 3
+          String[][] paragraph = new String[lines][];
+          for (int i = 0; i < paragraph.length; i++) {
+               System.out.print("line " + i + ": ");
+               String line = input.nextLine();
+               paragraph[i] = line.split("\\s");
+          }
+          for (int i = 0; i < paragraph.length; i++)
+          {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    System.out.print(paragraph[i][j] + " ");
+                    }
+                    System.out.println();
+          }
+
+          //Calculations and Output part 1
+          System.out.println("*** Part 1 ***");
+          for (int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    paragraph[i][j] = String.format("%-10s", paragraph[i][j]);
+               }
+          }
+          for (int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    System.out.print(paragraph[i][j] + " ");
+               }
+               System.out.println();
+          }
+        
+          
+          //Calculations and Output part 2
+          System.out.println("*** Part 2 ***");
+          for (int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    paragraph[i][j] = paragraph[i][j].substring(0,10);
+               }
+          }
+          for (int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    System.out.print(paragraph[i][j] + " ");
+               }
+               System.out.println();
+          }
+
+          //Calculations and Output part 3
+          System.out.println("*** Part 3 ***");
+          for(int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length - 1; j++) {
+                    for (int k = j + 1; k < paragraph[i].length; k++) {
+                         if (paragraph[i][j].compareToIgnoreCase(paragraph[i][k]) > 0) {
+                              String temp = paragraph[i][j];
+                              paragraph[i][j] = paragraph[i][k];
+                              paragraph[i][k] = temp;
+                         }
+                    }
+               }
+          }
+          for (int i = 0; i < paragraph.length; i++) {
+               for (int j = 0; j < paragraph[i].length; j++) {
+                    System.out.print(paragraph[i][j] + " ");
+               }
+               System.out.println();
+          }
+
+
+     }
+}
